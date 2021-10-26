facfe1bbb172e1bd9edc9e262e0d15ff3e750ed9
TashwillAndries <tashwilla27@gmail.com>
2021-10-26 09:14:42 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="navbar navbar-expand-lg py-3 fixed-top">
+    <div class="container">
+      <a href="#" class="navbar-brand text-dark">Bloggers</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navmenu"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navmenu">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link
+              :to="{ name: 'Login' }"
+              class="nav-link login-link text-dark"
+              >Login</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              :to="{ name: 'Signup' }"
+              class="nav-link signup-link text-dark"
+              >Signup</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.nav-link.login-link {
+  padding: 5px 10px;
+  background: green;
+  margin-right: 25px;
+}
+
+.nav-link.signup-link {
+  padding: 5px 10px;
+  background: blue;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Welcome from "../views/Welcome.vue";
+import Login from "../views/LoginForm.vue";
+import Signup from "../views/SignupForm.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Welcome",
+    component: Welcome,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
