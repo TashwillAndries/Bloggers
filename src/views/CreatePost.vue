44f77450de55420f4b1ffe114aba89627f0685d9
Koumori97 <d.e.fledermaus86@gmail.com>
2021-10-27 16:10:57 +0200

    create post update

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,27 +1,33 @@
 <template>
+<div class="welcome container">
     <form @submit.prevent="handleSubmit">
         <h4>Create New BlogPost</h4>
         <br>
-        <input 
-            type="text" 
-            required 
-            placeholder="Blog Title" 
-            v-model="title">
+        <div class="input-group input-group-lg">
+            <input 
+                type="text" 
+                required 
+                placeholder="Blog Title" 
+                v-model="title"
+                class="form-control shadow-none">
+        </div>
         <textarea 
+            class="form-control shadow-none"
             required
             placeholder="Blog Content..."
             v-model="content">
         </textarea>
         <label>Upload Banner Image</label>
+        <img class="preview" :src="picture">
+        <input type="file" @change="handleChange">
         <p>Progress: {{ uploadValue.toFixed() + "%" }}
             <progress style="display: none" :value="uploadValue" max="100"></progress>
         </p>
-        <img class="preview" :src="picture">
-        <input type="file" @change="handleChange">
         <div class="error">{{ fileError }}</div>
         <div class="error"></div>
-        <button @click="onUpload">Create</button>
+        <button class="text-decoration-none btn btn-info" @click="onUpload">Create</button>
     </form>
+</div>
 </template>
 
 <script>
@@ -82,21 +88,39 @@ export default {
 </script>
 
 <style>
-    form {
-        display: flex;
-        flex-direction: column;
-        margin: 200px 500px;
-    }
-    input[type="file"] {
-        border: 0;
-        padding: 0;
-    }
-    label {
-        font-size: 12px;
-        display: block;
-        margin-top: 30px;
-    }
-    button {
-        margin-top: 20px;
-    }
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+.welcome form {
+  width: 300px;
+  margin: 20px auto;
+}
+.welcome label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid #eee;
+  outline: none;
+  color: #999;
+  margin: 10px auto;
+}
+.welcome span {
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.welcome button {
+  margin: 20px auto;
+  width: 260px;
+}
+span {
+  margin-right: 15px;
+  padding: 8px 20px;
+  border-radius: 15px;
+}
 </style>
